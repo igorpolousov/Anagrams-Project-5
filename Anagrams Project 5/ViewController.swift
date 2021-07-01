523fa2f09859b555409f8120fc8ee16a4e20ef8e
Igor Polousov <igorpolousov@gmail.com>
2021-07-01 14:28:53 +0300

    Initial Commit

---
+++ Anagrams Project 5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anagrams Project 5
+//
+//  Created by Igor Polousov on 01.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
